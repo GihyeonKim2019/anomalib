03ad6917f913b01f8bc869e2b92cfe49995d76f8
GihyeonKim2019 <odohah@kaist.ac.kr>
2022-05-23 23:10:49 +0900

    Update torch_model.py

--- anomalib/models/patchcore/torch_model.py
+++ anomalib/models/patchcore/torch_model.py
@@ -166,9 +166,9 @@ class PatchcoreModel(DynamicBufferModule, nn.Module):
         """
         #distances = torch.cdist(embedding, self.memory_bank, p=2.0)  # euclidean norm
         #just comments
-        embeddingnormed = torch.nn.functional.normalize(embedding, p = 2.0, dim = 1, eps = 1e-12, out = None)
-        memorybanknormed = torch.nn.functional.normalize(self.memory_bank, p = 2.0, dim = 1, eps = 1e-12, out = None)
-        distances = torch.from_numpy(distance.cdist(embeddingnormed, memorybanknormed, 'cosine'))
+        #embeddingnormed = torch.nn.functional.normalize(embedding, p = 2.0, dim = 1, eps = 1e-12, out = None)
+        #memorybanknormed = torch.nn.functional.normalize(self.memory_bank, p = 2.0, dim = 1, eps = 1e-12, out = None)
+        distances = torch.from_numpy(distance.cdist(embedding, self.memory_bank, 'euclidean'))
 
         patch_scores, _ = distances.topk(k=n_neighbors, largest=False, dim=1)
         return patch_scores
